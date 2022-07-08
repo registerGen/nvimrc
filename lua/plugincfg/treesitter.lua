6e9839cd1f3f93711a1ad856ffb8420acd308628
registerGen <registergen@163.com>
2022-07-08 18:38:31 +0800

    feat(treesitter): auto install missing parsers
    nvim-treesitter/nvim-treesitter@365f0eb

--- lua/plugincfg/treesitter.lua
+++ lua/plugincfg/treesitter.lua
@@ -10,6 +10,7 @@ M.config = function()
 
   require('nvim-treesitter.configs').setup {
     ensure_installed = 'all',
+    auto_install = true,
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = false,
