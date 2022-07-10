e0688e334b35a44069973136f84e157a12d3100d
registerGen <registergen@163.com>
2022-07-10 19:18:53 +0800

    feat(colorscheme): disable onedarkpro by default

--- lua/plugincfg/onedarkpro.lua
+++ lua/plugincfg/onedarkpro.lua
@@ -1,18 +1,18 @@
 local M = {}
 
 M.config = function()
-  local onedarkpro = require 'onedarkpro'
-  onedarkpro.setup {
-    options = {
-      bold = true,
-      italic = true,
-      underline = true,
-      undercurl = true,
-      cursorline = true,
-      window_unfocussed_color = true,
-    },
-  }
-  -- onedarkpro.load()
+  if false then
+    require('ondarkpro').setup {
+      options = {
+        bold = true,
+        italic = true,
+        underline = true,
+        undercurl = true,
+        cursorline = true,
+        window_unfocussed_color = true,
+      },
+    }
+  end
 end
 
 return M
